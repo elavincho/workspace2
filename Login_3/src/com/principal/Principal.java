0b037580b85f34146208ebb41461eba575d76bd4
elavincho <127167994+elavincho@users.noreply.github.com>
2023-09-03 19:37:47 -0300

    Login 3 - No más interfaces feas!!

---
+++ Login_3/src/com/principal/Principal.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.principal;
+
+import com.igu.Login_3;
+
+/**
+ *
+ * @author elavi
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Login_3 login = new Login_3();
+        login.setVisible(true);
+        login.setLocationRelativeTo(null);
+    }
+    
+}
